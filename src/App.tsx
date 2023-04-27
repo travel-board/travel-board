4581e478088fe8bcc468e2b2cb2583fb63234d34
AJ <56358715+FenixKing@users.noreply.github.com>
2023-04-27 20:37:33 -0300

    create: input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Input } from "./components/Input"
+
+import { FaSearch } from 'react-icons/fa'
 
 
 function App() {
 
   return (
     <>
+      <Input type="text" placeholder="Digite seu email" button={false} error={false} message="error">
+        <FaSearch/>
+      </Input>
+      
     </>
   )
 }
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,27 @@
+import { BlockInput, Inputs } from "./styles"
+
+interface IInput {
+    type: string,
+    placeholder: string,
+    button: boolean,
+    error?: boolean;
+    message?: string,
+    register?: any,
+    children: React.ReactNode
+}
+
+export const Input = ({ type, placeholder, button, error, message, children}:IInput) => {
+    return(
+        <BlockInput>
+            <Inputs>
+                <input type={type} placeholder={placeholder}/>
+                {
+                    button ? <button>{children}</button> : null
+                }
+            </Inputs>
+            {
+                error ? <span>{message}</span> : null
+            }
+        </BlockInput>
+    )
+}
